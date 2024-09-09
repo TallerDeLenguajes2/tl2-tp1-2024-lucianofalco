f9b1eb2b1270e04635f8c62227b8560b0aa3b5ca
luciano <lucianosebastianfalco@gmail.com>
2024-09-09 09:25:11 -1200

    merge con rama tp2

--- obj/Debug/net8.0/tl2-tp1-2024-lucianofalco.AssemblyInfo.cs
+++ obj/Debug/net8.0/tl2-tp1-2024-lucianofalco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tl2-tp1-2024-lucianofalco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68bd3fe81fcf7113bf518f200fe6a5a1a8efb4e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7040b21377bda3fa69c5b29e43a05c16d4ad5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("tl2-tp1-2024-lucianofalco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tl2-tp1-2024-lucianofalco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
